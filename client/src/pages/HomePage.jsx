a8cbaf92d0f4852ef20b5e8dd820948eda80fe5b
sarud14 <ruzddchan@yahoo.com>
2025-07-22 14:14:01 +0700

    New feature/LandingUiByRuj

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,25 +1,14 @@
-import Footer from "../layouts/Footer"
-import CategorySection from "./home/CategorySection.jsx"
-import FeaturedProduct from "./home/FeaturedProduct.jsx"
-import HeroSection from "./home/HeroSection.jsx"
-import ProductList from "./home/ProductList.jsx"
-
-
+import HeroSection from "./Landing/HeroSection"
 
 
 function HomePage() {
   return (
     <div className="bg-pri-wh">
-     
       <main>
         <HeroSection />
-        <CategorySection />
-        <FeaturedProduct />
-        <ProductList />
       </main>
-      <Footer />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
