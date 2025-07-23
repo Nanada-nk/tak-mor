bd2282d793239935f9101e079f9c52be1394daf6
Nanada-nk <nada.nidchakul@gmail.com>
2025-07-23 21:07:24 +0700

    Update feture/RegisterFacebook byNada

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,27 +1,3 @@
-// 1. นำเข้า apiClient ที่เราสร้างไว้
-import apiClient from "./apiClient";
 
-const authApi = {};
 
-// 2. ใช้ apiClient เรียกไปยัง endpoint ต่างๆ
-authApi.register = (body) => {
-  return apiClient.post("/auth/register", body);
-};
 
-authApi.login = (body) => {
-  return apiClient.post("/auth/login", body);
-};
-
-authApi.getMe = () => {
-  return apiClient.get("/auth/me");
-};
-
-authApi.forgotPassword = (body) => {
-  return apiClient.post("/auth/forgot-password", body);
-};
-
-authApi.resetPassword = (body) => {
-  return apiClient.post("/auth/reset-password", body);
-};
-
-export default authApi;
--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -7,6 +7,7 @@ import FormInput from "../../components/FormInput.jsx";
 import authApi from "../../api/authApi.js";
 import AuthLayout from "../../components/auth/AuthLayout.jsx";
 import AuthFormCard from "../../components/auth/AuthFormCard.jsx";
+import { FacebookIcon, GoogleIcon } from "../../components/icons/index.jsx";
 
 
 function RegisterPage() {
@@ -23,23 +24,29 @@ function RegisterPage() {
     mode: 'onBlur'
   });
 
-  const onSubmit = async (data) => {
-    try {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      await authApi.register(data);
-      // console.log('resp', resp);
-      toast.success("Registration successful! Please log in.");
-      reset();
-      navigate("/login");
-    } catch (error) {
-      // console.error("Registration failed:", error);
-      toast.error(error.response?.data?.message || "Registration failed.");
-    }
-  };
+  // const onSubmit = async (data) => {
+  //   try {
+  //     await new Promise(resolve => setTimeout(resolve, 1000))
+  //     await authApi.register(data);
+  //     // console.log('resp', resp);
+  //     toast.success("Registration successful! Please log in.");
+  //     reset();
+  //     navigate("/login");
+  //   } catch (error) {
+  //     // console.error("Registration failed:", error);
+  //     toast.error(error.response?.data?.message || "Registration failed.");
+  //   }
+  // };
+
+
+
+
+
 
 
   return (
-    // <AuthLayout>
+    <>
+      {/* // <AuthLayout> */}
       <AuthFormCard
         title={["Register"]}
         onSubmit={handleSubmit(onSubmit)}
@@ -100,8 +107,14 @@ function RegisterPage() {
           placeholder="Confirm Password"
         />
 
-      </AuthFormCard>
-    // </AuthLayout>
+
+
+      </AuthFormCard >
+
+     
+      {/* // </AuthLayout> */}
+
+    </>
   )
 }
 
