0bbb196c7adc8bf307d9a31ce808eaa3cdd8ca07
sarud14 <ruzddchan@yahoo.com>
2025-07-25 17:27:53 +0700

    Update feature/AboutUsByRuj

--- client/src/pages/ContactUsPage.jsx
+++ client/src/pages/ContactUsPage.jsx
@@ -1,6 +1,25 @@
 function ContactUsPage() {
   return (
-    <div>ContactUsPage</div>
-  )
+    <div>
+      <div>
+        <p>ติดต่อเรา</p>
+        <p>มีคำถามใด ๆ ไหม?</p>
+      </div>
+      <fieldset className="fieldset bg-[#F6FAFF] border-base-300 rounded-box w-xs border p-4">
+        <label className="label">Title</label>
+        <input type="text" className="input" placeholder="My awesome page" />
+
+        <label className="label">Slug</label>
+        <input type="text" className="input" placeholder="my-awesome-page" />
+
+        <label className="label">Author</label>
+        <input type="text" className="input" placeholder="Name" />
+
+        <button className="btn bg-[#0E82FD] text-white w-25 rounded-full">
+          ส่ง
+        </button>
+      </fieldset>
+    </div>
+  );
 }
-export default ContactUsPage
+export default ContactUsPage;
