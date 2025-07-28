817a890d54545e16c583c7029bb3ce0e554f3772
sarud14 <ruzddchan@yahoo.com>
2025-07-28 12:07:59 +0700

    Update HeaderNavBar.jsx
    update gap

--- client/src/components/Navbar/HeaderNavBar.jsx
+++ client/src/components/Navbar/HeaderNavBar.jsx
@@ -33,7 +33,7 @@ function HeaderNavBar() {
   };
 
   return (
-    <div className="navbar flex justify-between w-[1350px] h-14 mt-2 mb-5">
+    <div className="navbar flex justify-between w-[1350px] h-14 mt-2 mb-5 gap-5">
       <a onClick={navigateHome} className="hover:cursor-pointer">
         <img
           src="../../public/takmor.svg"
