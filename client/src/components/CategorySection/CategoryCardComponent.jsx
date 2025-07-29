ff1cb32c4e33a6fe29cccd6429a9716bbbbb3b39
sarud14 <ruzddchan@yahoo.com>
2025-07-29 10:47:23 +0700

    Update and Fix for responsive By Ruz

--- client/src/components/CategorySection/CategoryCardComponent.jsx
+++ client/src/components/CategorySection/CategoryCardComponent.jsx
@@ -13,6 +13,8 @@ function CategoryCardComponent({
   itemsClass = "items-center",
   gapClass = "gap-4",
   fontWeightClass = "font-normal",
+  fontSize="text-sm",
+  lineHeight = "",
 }) {
   const navigate = useNavigate();
   const subtitle =
@@ -22,11 +24,11 @@ function CategoryCardComponent({
       className={`rounded-xl shadow-xl hover:cursor-pointer group flex ${widthClass} ${heightClass} ${flexClass} ${justifyClass} ${itemsClass} ${gapClass} p-4 transition-transform duration-200 hover:scale-[1.02]`}
       onClick={() => navigate(path)}
     >
-      <div className="w-10 h-10 rounded-full flex justify-center items-center text-[#6938EF] bg-[#E2EDFF] group-hover:bg-[#FCEFEA] flex-shrink-0">
-        <img src={iconSrc} alt={title} className="w-20 h-20 object-contain" />
+      <div className="w-15 h-15 rounded-full flex justify-center items-center text-[#6938EF] bg-[#E2EDFF] group-hover:bg-[#FCEFEA] flex-shrink-0">
+        <img src={iconSrc} alt={title} className="w-10 h-10 object-contain" />
       </div>
       <div className="flex flex-col gap-1 text-center">
-        <p className={`text-sm sm:text-base mt-1 ${fontWeightClass}`}>{title}</p>
+        <p className={`${fontSize} mt-1 ${lineHeight} ${fontWeightClass}`}>{title}</p>
         {showSubtitle && <p className="text-xs text-gray-500">{subtitle}</p>}
       </div>
     </div>
