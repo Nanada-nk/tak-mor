aa292aca45d3c259018070c012f6d0db206c0853
Nanada-nk <nada.nidchakul@gmail.com>
2025-08-01 21:39:05 +0700

    UpdateImportNameByNada

--- server/src/controllers/doctor.controller.js
+++ server/src/controllers/doctor.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../config/prisma.config";
+import prisma from "../config/prisma.config.js";
 
 export const getAllDoctors = async (req, res, next) => {
   try {
--- server/src/controllers/specialty.controller.js
+++ server/src/controllers/specialty.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../config/prisma.config";
+import prisma from "../config/prisma.config.js";
 
 export const getAllSpecialties = async (req, res, next) => {
   try {
